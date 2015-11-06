cdd82c8df718342d9eddaf15d76acf6f24f52a44
tekktronix <tekktronix@users.noreply.github.com>
2015-11-05 21:12:28 -0500

    Added files that were accidentally left out in initial check in

---
+++ src/main/java/com/tekktronix/utils/ErrorStrings.java
@@ -0,0 +1,38 @@
+/*
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c)  ${YEAR} Tekktronix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package com.tekktronix.utils;
+
+/**
+ * Created by Tekktronix on 10/25/2015.
+ *
+ * Contains all the error strings used in this project
+ */
+public class ErrorStrings {
+    public final static String NullArgument = "The \"{0}\" parameter should not be null.";
+
+    public final static String AttemptToDivideByZero = "Attempt to divide by zero. The parameter \"{0}\" should not be zero.";
+}
